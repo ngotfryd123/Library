f2067469ea1ee4e9debb8d5012f93898f4eb899b
ngotfryd123 <nick_gotfryd@hotmail.com>
2023-09-05 13:01:22 -0700

    Added back-img, and functions

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-function Player(name, marker) {
-    this.name = name
-    this.marker = marker
-    this.sayName = function() {
-      console.log(name)
+const myLibrary = [];
+
+function Book(title,author,pages,read){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+    this.info = function(){
+        return(title+" by "+author+", "+pages+" pages, "+read);
     }
+}
+
+function addBookToLibrary() {
+
+    const book = new Book()
+    book.title = prompt("Please enter the title: ");
+    book.author = prompt("Please enter the author: ");
+    book.pages = prompt("Please enter the amount of pages: ");
+
+    myLibrary.push(book);
+    console.log(myLibrary);
+
   }
-  
-  const player1 = new Player('steve', 'X')
-  const player2 = new Player('also steve', 'O')
-  player1.sayName() // logs 'steve'
-  player2.sayName() // logs 'also steve'
+
+function displayBook(){
+    //loop through array and display each book
+    //display on table or on their own card
+    //helps to add a few books manually in the array to see how they display
+}
+
+//add a BUTTON for adding a new book
+//this will bring up a form for the user to input
+//submitting a form will send to a server and cause an issue read more for details
+
+  addBookToLibrary();
+
