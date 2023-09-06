b1aa766f3335713e1e3081ca9e11c1b4d6cd1b53
ngotfryd123 <nick_gotfryd@hotmail.com>
2023-09-06 12:12:47 -0700

    added book object info to dom books

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ function addBookToLibrary() {
     book.pages = prompt("Please enter the amount of pages: ");
 
     myLibrary.push(book);
-    console.log(myLibrary);
 
   }
 
@@ -26,6 +25,18 @@ function displayBook(){
     //loop through array and display each book
     //display on table or on their own card
     //helps to add a few books manually in the array to see how they display
+    
+for(let i = (myLibrary.length-1); i >-1; i-- ){
+
+        const books = document.createElement("div");
+        const container = document.getElementById("book-container");
+        container.appendChild(books);
+        books.style.backgroundImage="url('istockphoto-647809752-612x612.jpg')";
+        let n = 0;
+        books.textContent = JSON.stringify(myLibrary[0+n]);
+        n++;
+}     
+    
 }
 
 //add a BUTTON for adding a new book
@@ -33,4 +44,4 @@ function displayBook(){
 //submitting a form will send to a server and cause an issue read more for details
 
   addBookToLibrary();
-
+displayBook();
