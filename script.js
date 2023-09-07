d49d34c61677f1d0b8655fe32187de824b3a8a62
ngotfryd123 <nick_gotfryd@hotmail.com>
2023-09-07 16:06:10 -0700

    fixed display loop and getting input values

--- script.js
+++ script.js
@@ -1,47 +1,59 @@
 const myLibrary = [];
+document.getElementById("form").style.display="none";
+
 
 function Book(title,author,pages,read){
     this.title=title;
     this.author=author;
     this.pages=pages;
     this.read=read;
-    this.info = function(){
-        return(title+" by "+author+", "+pages+" pages, "+read);
     }
-}
-
-function addBookToLibrary() {
-
-    const book = new Book()
-    book.title = prompt("Please enter the title: ");
-    book.author = prompt("Please enter the author: ");
-    book.pages = prompt("Please enter the amount of pages: ");
-
-    myLibrary.push(book);
-
-  }
 
 function displayBook(){
     //loop through array and display each book
     //display on table or on their own card
     //helps to add a few books manually in the array to see how they display
-    
-for(let i = (myLibrary.length-1); i >-1; i-- ){
+  document.getElementById("book-container").innerHTML="";
+  for(let i = (myLibrary.length-1); i >-1; i-- ){
+
+        let book = myLibrary[i];
+        const container = document.getElementById("book-container");
 
         const books = document.createElement("div");
-        const container = document.getElementById("book-container");
         container.appendChild(books);
         books.style.backgroundImage="url('istockphoto-647809752-612x612.jpg')";
-        let n = 0;
-        books.textContent = JSON.stringify(myLibrary[0+n]);
-        n++;
-}     
-    
+        const btitle = document.createElement("div");
+        const bauthor = document.createElement("div");
+        const bpages = document.createElement("div");
+        const bread = document.createElement("div");
+        books.appendChild(btitle);
+        btitle.innerHTML = `<p>${book.title}</p>`;
+        books.appendChild(bauthor);
+        bauthor.innerHTML = `<p>${book.author}</p>`;
+        books.appendChild(bpages);
+        bpages.innerHTML = `<p>${book.pages}</p>`;
+        books.appendChild(bread);
+        bread.innerHTML = `<p>${book.read}</p>`;
+}}
+
+function addBookToLibrary() {
+
+   let title = document.getElementById('title').value
+   let author = document.getElementById('author').value
+   let pages = document.getElementById('pages').value
+   let read = document.getElementById('read').checked
+   const book = new Book(title,author,pages,read)
+  myLibrary.push(book);
+  displayBook();
+   }
+
+  document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    addBookToLibrary();})
+ 
+function asd(a)
+{
+    if(a==1)
+        document.getElementById("form").style.display="block";
 }
 
-//add a BUTTON for adding a new book
-//this will bring up a form for the user to input
-//submitting a form will send to a server and cause an issue read more for details
-
-  addBookToLibrary();
-displayBook();
