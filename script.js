9c2ed2a5fad2eab2aa429aa269cbb216feed39da
ngotfryd123 <nick_gotfryd@hotmail.com>
2023-10-20 13:09:34 -0700

    Changed constructor to a class

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const myLibrary = [];
 document.getElementById("form").style.display="none";
 
-function Book(title,author,pages,read){
+class Book{
+  constructor(title,author,pages,read){
     this.title=title;
     this.author=author;
     this.pages=pages;
     this.read=read;
     }
+  }
 
 Book.prototype.toggleRead= function(){
   this.read=!this.read;
